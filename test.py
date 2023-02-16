120d499a365f299bbddf942de903e8313a00d043
unknown <xbx18502@gmail.com>
2023-02-16 15:41:21 +0900

    try combine model ge and cuda  kernel

---
+++ test.py
@@ -0,0 +1,5 @@
+import os
+# my_file = Path('.\model_gen_mc_slab\\x64\\Debug\\model_gen_mc_slab.exe')
+# print(my_file.exists())
+os.chdir('D:\\files2\\Nagoya\\python\\kiwi_ToF_simulation\\slab_dataset_generator')
+print(os.path.isfile('.\model_gen_mc_slab\\x64\\Debug\\model_gen_mc_slab.exe'))
---
+++ test_pointer.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+#include <tuple>
+#define N 1000
+int* model_generator(int* T){
+    T = (int*)malloc(sizeof(int)*N);
+    for(int i=0;i<N;i++){
+        *(T+i) = i;
+    }
+    return T;
+}
+
+int main(){
+    int* T1 = nullptr;
+    T1 = model_generator(T1);
+    for(int i=0;i<N;i++){
+        std::cout<<*(T1+i)<<"\n";
+    }
+
+}
+
